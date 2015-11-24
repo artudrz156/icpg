6099d21461c8aa17791f725e7ef1a67e0c522378
artudrz156 <artudrz156@student.polsl.pl>
2015-11-24 20:51:54 +0100

    Change view of user dashboard
    Change view of user dashboard.
    The galery load script has been added to user dashboard.
    Sample images added to project.

--- user_dashboard.php
+++ user_dashboard.php
@@ -7,21 +7,63 @@
 		<link rel="stylesheet" type="text/css" href="styles.css" />
 	</head>
 	<body>
-		<table border="0" cellpadding="10" cellspacing="1" width="500" align="center">
-			<tr class="tableheader">
-				<td align="center">User Dashboard</td>
-			</tr>
-			<tr class="tablerow">
-				<td>
+		<div id="mainUserGallery">
+			<div id="header">
+				<div id="userDetails">
+					<?php
+						if($_SESSION["username"]) {
+					?>
+						Welcome <?php echo $_SESSION["username"]; ?> | <a href="logout.php" tite="Logout">Logout.</a>
+					<?php
+						}
+					?>
+				</div>
+				<div id="mainHeader">
+					<h1>Incredible Complicated User Gallery</h1>
+				</div>				
+			</div>
+			<div id="menu">
+				<ul>
+					<li><a href='index.php'>Strona główna</a></li>
+					<?php	
+						if ($foldery = opendir('zdjecia/')) {
+							while (($dir = readdir($foldery))!=false) { 
+								if(!ereg(".gif$|.jpg$", $dir)) {
+									if ($dir != "." && $dir != "..") {	
+										echo "<li><a href='user_dashboard.php?name=$dir'>$dir</a></li>";
+									}
+								}
+							}
+						}
+						closedir($foldery);
+					?>
+				</ul>
+			</div>
+			<div id="zawartosc">
 				<?php
-				if($_SESSION["username"]) {
+					$A = null;
+					if (isset($_GET['name'])) {
+						$A = $_GET['name'];
+					}
+					if ($obrazy = opendir('zdjecia/'.$A.'/')) {
+						while (($plik = readdir($obrazy))!=false) {
+							if(ereg(".gif$|.jpg$", $plik)) {		
+								echo "<a href='zdjecia/$A/$plik' rel='lightbox[]'> <div id='zdjeciecale'>
+										<div id='zdjecie'><img src='zdjecia/$A/$plik' /></div> 
+										<div id='podpis'>$plik</div>
+										</div></a>";
+							}	
+						}
+						closedir($obrazy);	
+					}
+					
+					
 				?>
-				Welcome <?php echo $_SESSION["username"]; ?>. Click here to <a href="logout.php" tite="Logout">Logout.
-				<?php
-				}
-				?>
-				</td>
-			</tr>
-		</table>
+			</div>
+			<div id="content>
+			
+			<div>
+		</div>
+		
 	</body>
 </html>
